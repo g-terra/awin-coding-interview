3214f960e65be26b9a9d47b52fa6b2e5e7550083
Guilherme Terra <by.terra@hotmail.com>
2021-03-01 22:37:09 +0100

    old today refactored and divided into 3 independent endpoints

--- src/main/java/com/awin/coffeebreak/controller/CoffeeBreakPreferenceController.java
+++ src/main/java/com/awin/coffeebreak/controller/CoffeeBreakPreferenceController.java
@@ -4,11 +4,14 @@ import com.awin.coffeebreak.entity.CoffeeBreakPreference;
 import com.awin.coffeebreak.entity.StaffMember;
 import com.awin.coffeebreak.repository.CoffeeBreakPreferenceRepository;
 import com.awin.coffeebreak.repository.StaffMemberRepository;
+import com.awin.coffeebreak.services.CoffeeBreakPreferenceService;
+import com.awin.coffeebreak.services.utils.CurrentDayCoffeeBreakPreferenceResponse;
 import com.awin.coffeebreak.services.SlackNotifier;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,48 +23,38 @@ public class CoffeeBreakPreferenceController {
 
     public CoffeeBreakPreferenceRepository coffeeBreakPreferenceRepository;
     public StaffMemberRepository staffMemberRepository;
+    public CoffeeBreakPreferenceService coffeeBreakPreferenceService;
 
-    public CoffeeBreakPreferenceController(
-          CoffeeBreakPreferenceRepository coffeeBreakPreferenceRepository
+    public CoffeeBreakPreferenceController(CoffeeBreakPreferenceRepository coffeeBreakPreferenceRepository, CoffeeBreakPreferenceService coffeeBreakPreferenceService
     ) {
         this.coffeeBreakPreferenceRepository = coffeeBreakPreferenceRepository;
+        this.coffeeBreakPreferenceService = coffeeBreakPreferenceService;
     }
 
-    /**
-     * Publishes the list of preferences in the requested format
-     */
-    @GetMapping(path = "/today")
-    public ResponseEntity<?> today(@RequestParam(name = "format" , required = false) String format) {
-        if (format == null) {
-            format = "html";
-        }
-
-        List<CoffeeBreakPreference> t = coffeeBreakPreferenceRepository.getPreferencesForToday();
-
-        String responseContent;
-        String contentType = "text/html";
-
-        switch (format) {
-            case "json":
-                responseContent = getJsonForResponse(t);
-                contentType = "application/json";
-                break;
-
-            case "xml":
-                responseContent = getXmlForResponse(t);
-                contentType = "text/xml";
-                break;
-
-            default:
-                String formattedPreferences = getHtmlForResponse(t);
-                return ResponseEntity.ok().contentType(MediaType.valueOf(contentType))
-                      .body(formattedPreferences);
-        }
-
+    @GetMapping(path = "/today" , produces={"application/json"})
+    public ResponseEntity<?> todayJson() {
+        CurrentDayCoffeeBreakPreferenceResponse todayCoffeeBreakAsJSON = coffeeBreakPreferenceService.getTodayCoffeeBreakAsJSON();
         return ResponseEntity.ok()
-              .contentType(MediaType.valueOf(contentType))
-              .body(responseContent);
+                .contentType(MediaType.valueOf(todayCoffeeBreakAsJSON.getContentType()))
+                .body(todayCoffeeBreakAsJSON.getContent());
     }
+
+    @GetMapping(path = "/today" , produces={"text/xml"})
+    public ResponseEntity<?> todayXml() {
+        CurrentDayCoffeeBreakPreferenceResponse todayCoffeeBreakAsXML = coffeeBreakPreferenceService.getTodayCoffeeBreakAsXML();
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(todayCoffeeBreakAsXML.getContentType()))
+                .body(todayCoffeeBreakAsXML.getContent());
+    }
+
+    @GetMapping(path = "/today" , produces={"text/html"})
+    public ResponseEntity<?> todayHtml() {
+        CurrentDayCoffeeBreakPreferenceResponse todayCoffeeBreakAsHTML = coffeeBreakPreferenceService.getTodayCoffeeBreakAsHTML();
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(todayCoffeeBreakAsHTML.getContentType()))
+                .body(todayCoffeeBreakAsHTML.getContent());
+    }
+
 
     @GetMapping("/notifyStaffMember")
     public ResponseEntity<Object> notifyStaffMember(@RequestParam("staffMemberId") int id) {
@@ -75,36 +68,4 @@ public class CoffeeBreakPreferenceController {
         return ResponseEntity.ok(ok ? "OK" : "NOT OK");
     }
 
-    private String getJsonForResponse(final List<CoffeeBreakPreference> list) {
-        String responseJson = "{\"preferences\":[";
-
-        for (final CoffeeBreakPreference p : list) {
-            responseJson += p.getAsJson();
-        }
-
-        return responseJson += "]}";
-    }
-
-    private String getXmlForResponse(List<CoffeeBreakPreference> list) {
-        String responseJson = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        responseJson += "<Preferences>";
-
-        for (final CoffeeBreakPreference p : list) {
-            responseJson += p.getAsXml();
-        }
-
-        responseJson += "</Preferences>";
-
-        return responseJson;
-    }
-
-    private String getHtmlForResponse(final List<CoffeeBreakPreference> list) {
-        String responseJson = "<ul>";
-
-        for (final CoffeeBreakPreference p : list) {
-            responseJson += p.getAsListElement();
-        }
-
-        return responseJson + "</ul>";
-    }
 }
