d4ed2335dc9be88075857b962e789d6d866dfc9e
Guilherme Terra <by.terra@hotmail.com>
2021-03-01 21:37:50 +0100

    WebFormat interface was created to work as a code contract to every class that can be provided as a response on the api endpoints so we can support multiple formats

---
+++ src/main/java/com/awin/coffeebreak/services/utils/formattingUtils/WebFormat.java
@@ -0,0 +1,13 @@
+package com.awin.coffeebreak.services.utils.formattingUtils;
+
+import java.util.stream.Collectors;
+
+public interface WebFormat {
+
+    String getAsJson();
+
+    String getAsXml();
+
+    String getAsListElement();
+
+}
