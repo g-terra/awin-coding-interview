59025aa9f570e4fe2fac7816b426eaf68713170b
Guilherme Terra <by.terra@hotmail.com>
2021-03-01 16:39:17 +0100

    Added test for method today() for class CoffeeBreakPreferenceController. get request with json as parameter returns json on body with all fields. PS. first test failed as the subtype is not being assigned to the feed on the coffeeBreakPreference object.

--- src/test/junit/com/awin/coffeebreak/controller/CoffeeBreakPreferenceControllerTest.java
+++ src/test/junit/com/awin/coffeebreak/controller/CoffeeBreakPreferenceControllerTest.java
@@ -1,21 +1,90 @@
 package com.awin.coffeebreak.controller;
 
+import com.awin.coffeebreak.entity.CoffeeBreakPreference;
+import com.awin.coffeebreak.entity.StaffMember;
+import com.awin.coffeebreak.repository.CoffeeBreakPreferenceRepository;
+import com.awin.coffeebreak.services.SlackNotifier;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class CoffeeBreakPreferenceControllerTest {
+@AutoConfigureMockMvc
+public class CoffeeBreakPreferenceControllerTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @MockBean
+    public CoffeeBreakPreferenceRepository coffeeBreakPreferenceRepository;
+
+    @InjectMocks
+    public CoffeeBreakPreferenceController coffeeBreakPreferenceController;
 
     @Test
-    void getRequestToTodaysPreferencesEndpoint_requestJSON_returnsJsonWithCorrecteData() {
+    public void getRequestToTodaysPreferencesEndpoint_requestJSON_returnsJsonWithCorrectData() throws Exception {
+
+
+        StaffMember expectedRequestedBy = new StaffMember();
+        expectedRequestedBy.setSlackIdentifier("ABC123");
+        String expectedType = "drink";
+        String expectedSubType = "coffee";
+        HashMap<String, String> expectedDetails = new HashMap<>();
+
+        CoffeeBreakPreference preference1 = new CoffeeBreakPreference(expectedType, expectedSubType, expectedRequestedBy, expectedDetails);
+
+        List<CoffeeBreakPreference> todayPreferences = Collections.singletonList(preference1);
+
+        Mockito.when(coffeeBreakPreferenceRepository.getPreferencesForToday()).thenReturn(todayPreferences);
+
+        URI uri = new URI("/today?format=json");
+
+        String expectedJsonResponseBody = String.format(
+                "{\"preferences\":[{\"id\":null," +
+                        " \"type\":\"%s\"," +
+                        " \"subType\":\"%s\"," +
+                        " \"requestedBy\":\"%s\"," +
+                        " \"requestedDate\":\"null\"," +
+                        " \"details\":\"%s\"" +
+                        "}]}"
+                , expectedType, expectedSubType, expectedRequestedBy.toString(), expectedDetails.size() == 0 ? "{}" : expectedDetails)
+                .replace(",\\", ", ");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(uri))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedJsonResponseBody));
 
 
     }
 
     @Test
-    void notifyStaffMember() {
+    public void notifyStaffMember() {
     }
 }
