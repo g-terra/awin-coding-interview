ee05b96874be04eeb02e3f625770e632bdb010ad
Guilherme Terra <by.terra@hotmail.com>
2021-03-01 21:45:05 +0100

    Formatting strategy for json response

---
+++ src/main/java/com/awin/coffeebreak/services/utils/formattingUtils/JSONFormattingStrategy.java
@@ -0,0 +1,22 @@
+package com.awin.coffeebreak.services.utils.formattingUtils;
+
+import com.awin.coffeebreak.entity.CoffeeBreakPreference;
+
+public class JSONFormattingStrategy implements FormattingStrategy<CoffeeBreakPreference> {
+
+    @Override
+    public String formatName() {
+        return "Application/json" ;
+    }
+
+    @Override
+    public String formatItem(WebFormat o) {
+        return o.getAsJson();
+    }
+
+    @Override
+    public String formatWrapper() {
+        return "{\"preferences\":[%s]}";
+    }
+
+}
