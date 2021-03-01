1dd20152d43d04b7c5634f1b7d114dd61065f633
Guilherme Terra <by.terra@hotmail.com>
2021-03-01 14:46:42 +0100

    Added directory for junit tests plus test class for coffee break controller

---
+++ src/test/junit/com/awin/coffeebreak/controller/CoffeeBreakPreferenceControllerTest.java
@@ -0,0 +1,21 @@
+package com.awin.coffeebreak.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class CoffeeBreakPreferenceControllerTest {
+
+    @Test
+    void getRequestToTodaysPreferencesEndpoint_requestJSON_returnsJsonWithCorrecteData() {
+
+
+    }
+
+    @Test
+    void notifyStaffMember() {
+    }
+}
